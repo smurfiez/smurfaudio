6582968031b4b7f1d73683349304f0e3a6a27e03
Smurfiez <5055445+smurfiez@users.noreply.github.com>
2026-09-15 23:48:36 +0800

    Bump version to 1.1.0

--- create_installer.sh
+++ create_installer.sh
@@ -34,7 +34,7 @@ pkgbuild \
     --root "$BUILD_ROOT" \
     --scripts "installer_scripts" \
     --identifier "com.smurfaudio.installer" \
-    --version "1.0.0" \
+    --version "1.1.0" \
     --install-location "/" \
     "$PACKAGE_NAME"
 
--- run_feature_tests.sh
+++ run_feature_tests.sh
@@ -59,7 +59,7 @@ proc.stdin.write(JSON.stringify({
   params: {
     protocolVersion: "2024-11-05",
     capabilities: {},
-    clientInfo: { name: "smurfaudio-tester", version: "1.0.0" }
+    clientInfo: { name: "smurfaudio-tester", version: "1.1.0" }
   }
 }) + "\n");
 '
